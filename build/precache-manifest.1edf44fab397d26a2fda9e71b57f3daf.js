4a38fd235bed41d89aa2adf8fd9b1e798fb4167d
kaustubhCodes <kaustubhpardhi006@gmail.com>
2020-06-18 22:54:00 +0530

    First commit

---
+++ build/precache-manifest.1edf44fab397d26a2fda9e71b57f3daf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f498877c3f984627b5d1aa0105d245da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b3cbb980160bbf343eb",
+    "url": "/static/css/main.4be92bae.chunk.css"
+  },
+  {
+    "revision": "a03d6d4a073f697aa463",
+    "url": "/static/js/2.65694226.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65694226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b3cbb980160bbf343eb",
+    "url": "/static/js/main.650d9f08.chunk.js"
+  },
+  {
+    "revision": "f4cd4718aca447991293",
+    "url": "/static/js/runtime-main.108b9577.js"
+  },
+  {
+    "revision": "91c109fc2e2a14e1e0a445d4314f9334",
+    "url": "/static/media/city.91c109fc.jpg"
+  }
+]);
